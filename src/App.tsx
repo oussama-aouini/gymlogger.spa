e07819883c8274b1f0c786b1cc5db965a76d8d64
Ousaama Aouini <oussama.aouini.dev@gmail.com>
2025-01-04 12:51:00 +0100

    type narrowing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import ExerciseList from "./components/ExerciseList/ExerciseList";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <ExerciseList />
+    </div>
+  );
 };
 export default App;
--- src/components/ExerciseList/ExerciseList.tsx
+++ src/components/ExerciseList/ExerciseList.tsx
@@ -1,21 +1,37 @@
 import { ChangeEvent, useState, SyntheticEvent } from "react";
+import { ExerciseDto } from "../../gym";
+import { searchExercises } from "../../api";
 
 type Props = {};
 const ExerciseList = (props: Props) => {
   const [search, setSearch] = useState<string>("");
+  // allways inzilise state with a value or you will get undefined errors
+  const [exercises, setExercises] = useState<ExerciseDto[]>([]);
+  const [serverError, setServerError] = useState<string>("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {};
 
-  const handleClick = (e: SyntheticEvent) => {};
+  const handleClick = async (e: SyntheticEvent) => {
+    // hover over result to see its type
+    const result = await searchExercises();
+    // this is typenarrowing meaning doing different things depending on the return type
+    // we do it when the server reponse can be of different types
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setExercises(result.data);
+    }
+    console.log(exercises);
+  };
 
   return (
     <div>
       <input value={search} onChange={(e) => handleChange(e)} />
-      <button onClick={(e) => handleClick(e)} />
+      <button onClick={(e) => handleClick(e)}>Get Exercises</button>
     </div>
   );
 };
 export default ExerciseList;
 
 // to get the type of event hover the cursor over the e passed in the event handler prop
-// if the type doesn't work use SyntheticEvent it is like the base class for all events 
+// if the type doesn't work use SyntheticEvent it is like the base class for all events
