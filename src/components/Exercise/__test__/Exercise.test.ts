7ec5a131e10fc0d6220a42f407d0d9b14ae59685
Oussama Aouini <oussama.aouini.dev@gmail.com>
2025-01-31 17:05:18 +0100

    first test

---
+++ src/components/Exercise/__test__/Exercise.test.ts
@@ -0,0 +1,5 @@
+// Test Block
+// 1- Render the component we want to test with RTL render method
+// 2- Find the elemets we want to interact with using RTL screen method
+// 3- interact with those elemets
+// 4- assert that the results are as expected
